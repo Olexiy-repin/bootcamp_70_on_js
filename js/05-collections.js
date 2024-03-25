f0f1711409cb88b83d2db4201b74364e15f74c7d
Olexiy-repin <repinlyoha@gmail.com>
2024-03-25 22:16:04 +0200

    preparation

---
+++ js/05-collections.js
@@ -0,0 +1,57 @@
+/*
+ * Створюємо та додаємо колекцію
+ */
+const pictures = [
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/0/700/460',
+    alt: 'laptop',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1/700/460',
+    alt: 'developer',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/10/700/460',
+    alt: 'forest',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/100/700/460',
+    alt: 'beach',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1000/700/460',
+    alt: 'mountain',
+  },
+];
+
+/*
+<li class="gallery-item">
+  <a href="#">
+    <img src="" alt="" width="" height="">
+  </a>
+</li>
+*/
+
+// const galleryListEl = document.querySelector('.js-gallery');
+
+//? Функція для створення карточки makeGalleryCard(cardInfo)
+const makeGalleryCard = pictureInfo => {
+  // Створення li
+  // Створення a
+  // Створення img
+};
+
+//? Перебір масиву pictures
+// const galleryCardsArr = null;
+
+//? Вставка колекції карток на сторінку
---
+++ js/06-templating.js
@@ -0,0 +1,50 @@
+const pictures = [
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/0/700/460',
+    alt: 'laptop',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1/700/460',
+    alt: 'developer',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/10/700/460',
+    alt: 'forest',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/100/700/460',
+    alt: 'beach',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1000/700/460',
+    alt: 'mountain',
+  },
+];
+
+/*
+<li class="gallery-item">
+  <a href="#">
+    <img src="" alt="" width="" height="">
+  </a>
+</li>
+*/
+
+// const galleryListEl = document.querySelector('.js-gallery');
+
+//? Функція для створення карточки makeGalleryCard(cardInfo)
+// const makeGalleryCard = pictureInfo => {};
+
+//? Створення масиву рядків із елементами
+// const cardsArr = null;
+
+//? Вставка елементів на сторінку
